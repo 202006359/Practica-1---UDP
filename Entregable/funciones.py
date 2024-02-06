2c0dd338c97fa7e8bd90d0af8ab15c31d71da7de
202006359 <202006359@alu.comillas.edu>
2024-02-06 12:04:38 +0100

    commit 2

--- Entregable/funciones.py
+++ Entregable/funciones.py
@@ -1,3 +1,7 @@
+import pickle
+import random
+import socket
+
 def ask_IP_address():
     valid_IP = False
     while not valid_IP:
@@ -30,3 +34,20 @@ def ask_message():
 def ask_number_client():
     number_client = random.randint(1, 1000)
     return number_client
+
+def send_to_server(message, number_client, ip_address, port_number, socket_cliente):
+    try:
+        # Send message to server
+        send_to_server = (message, number_client)
+        socket_cliente.sendto(pickle.dumps(send_to_server), (ip_address, port_number))
+    except Exception as e:
+        print(f"Client: Error occurred while sending message to server: {e}")
+    
+def receive_from_server(socket_cliente):
+    try:
+        # Receive message from server
+        bytes_rx, _ = socket_cliente.recvfrom(1024)
+        return bytes_rx
+    except Exception as e:
+        print(f"Client: Error occurred while receiving message from server: {e}")
+        return None
